63fd0d62f56dbe0898dbaa3a79ba736148d70467
karthikrt09 <karthikrt.rt@gmail.com>
2017-05-13 21:27:01 -0700

    base file

---
+++ paintingCollector/PaintingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PaintingViewController.swift
+//  paintingCollector
+//
+//  Created by Karthik Thirunavukkarasan on 5/12/17.
+//  Copyright © 2017 Karthik Thiru. All rights reserved.
+//
+
+import UIKit
+
+class PaintingViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var paintingImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+
+    @IBAction func photoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        <#code#>
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
